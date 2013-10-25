c4fe5a3d4c565df45bfcfc636a3879508cf00c20
Timothy du Heaume <timothy.duheaume@gmail.com>
2013-10-26 00:14:16 +0800

    let's try finding classes...

--- Vinyl.Transformer/Program.cs
+++ Vinyl.Transformer/Program.cs
@@ -1,11 +1,21 @@
 using System;
+using System.Linq;
 
 namespace Vinyl.Transformer
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-		}
-	}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = Mono.Cecil.AssemblyDefinition.ReadAssembly(args[0]);
+
+            foreach (var t in from module in a.Modules
+                              from type in module.GetTypes()
+                              where type.CustomAttributes.Any(x => x.AttributeType.FullName == typeof(Vinyl.RecordAttribute).FullName)
+                              select type)
+            {
+                Console.WriteLine(t.FullName);
+            }
+        }
+    }
 }
