47fe78de56e957028d512ef1448b3fa2263e2a46
Timothy du Heaume <timothy.duheaume@gmail.com>
2013-10-26 04:33:28 +0800

    set fields in constructor

--- Vinyl.Transformer/Program.cs
+++ Vinyl.Transformer/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace Vinyl.Transformer
 {
@@ -15,9 +16,10 @@ namespace Vinyl.Transformer
                               where type.CustomAttributes.Any(x => x.AttributeType.FullName == typeof(Vinyl.RecordAttribute).FullName)
                               select type)
             {
-                var constructor = (from method in t.Methods
+                MethodDefinition constructor = (from method in t.Methods
                                    where method.IsConstructor
                                    select method).Single();
+                int argCount = 0;
                 foreach (var param in constructor.Parameters)
                 {
                     var fieldName = StringUtils.FirstCharToUpper(param.Name);
@@ -25,8 +27,24 @@ namespace Vinyl.Transformer
                                                     FieldAttributes.Public |
                                                     FieldAttributes.InitOnly,
                                                     param.ParameterType);
+                    ++argCount;
+                    Instruction loadArg;
+                    if (argCount == 1)
+                        loadArg = Instruction.Create(OpCodes.Ldarg_1);
+                    else if (argCount == 2)
+                        loadArg = Instruction.Create(OpCodes.Ldarg_2);
+                    else if (argCount == 3)
+                        loadArg = Instruction.Create(OpCodes.Ldarg_3);
+                    else
+                        loadArg = Instruction.Create(OpCodes.Ldarg_S, argCount);
+
                     t.Fields.Add(field);
-                    Console.WriteLine(param.Name);
+                    var instructions = constructor.Body.Instructions;
+
+                    instructions.InsertRange(instructions.Count - 1,
+                        Instruction.Create(OpCodes.Ldarg_0),
+                        loadArg,
+                        Instruction.Create(OpCodes.Stfld, field));
                 }
 
                 a.Write(args[0]);
