3f562d546bfc5210e24a931ba087e8bb8a833f78
Timothy du Heaume <timothy.duheaume@gmail.com>
2013-10-26 17:05:14 +0800

    transform the test dll as part of SetUp

--- Vinyl.Tests/Test.cs
+++ Vinyl.Tests/Test.cs
@@ -1,23 +1,29 @@
-using NUnit.Framework;
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using NUnit.Framework;
 
 namespace Vinyl.Tests
 {
     [TestFixture()]
     public class Test
     {
+        private Assembly testAssembly;
+
         [SetUp()]
         public void SetUp()
         {
+            var outputDirectory = "Test.dll";
             var a = Assembly.GetExecutingAssembly();
             var s = a.GetManifestResourceStream("Vinyl.Tests.Test.dll");
-            using (var sw = File.Create("Test.dll"))
+            using (var sw = File.Create(outputDirectory))
             {
                 s.Seek(0, SeekOrigin.Begin);
                 s.CopyTo(sw);
             }
+            Vinyl.Transformer.MainClass.Main(new [] {outputDirectory});
+            testAssembly = Assembly.LoadFile(outputDirectory);
         }
 
         [Test()]
