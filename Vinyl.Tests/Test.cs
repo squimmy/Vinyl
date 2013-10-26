ec0c57423380274d06b3f27082355dc490f566b2
Timothy du Heaume <timothy.duheaume@gmail.com>
2013-10-26 14:48:40 +0800

    SetUp and TearDown the test dll

--- Vinyl.Tests/Test.cs
+++ Vinyl.Tests/Test.cs
@@ -1,14 +1,34 @@
 using NUnit.Framework;
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace Vinyl.Tests
 {
     [TestFixture()]
     public class Test
     {
+        [SetUp()]
+        public void SetUp()
+        {
+            var a = Assembly.GetExecutingAssembly();
+            var s = a.GetManifestResourceStream("Vinyl.Tests.Test.dll");
+            using (var sw = File.Create("Test.dll"))
+            {
+                s.Seek(0, SeekOrigin.Begin);
+                s.CopyTo(sw);
+            }
+        }
+
         [Test()]
         public void TestCase()
         {
+        }
+
+        [TearDown()]
+        public void TearDown()
+        {
+            File.Delete("Test.dll");
         }
     }
 }
