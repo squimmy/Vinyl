74be1d725f2fae9ca6f42580a170fcf7eb3306b9
Timothy du Heaume <timothy.duheaume@gmail.com>
2013-11-07 02:34:42 +0800

    fix indentation

--- Vinyl.Transformer/CollectionExtensions.cs
+++ Vinyl.Transformer/CollectionExtensions.cs
@@ -3,15 +3,15 @@ using Mono.Collections.Generic;
 
 namespace Vinyl.Transformer
 {
-	public static class CollectionExtensions
-	{
-		public static void InsertRange<T>(this Collection<T> collection, int index, IEnumerable<T> range)
-		{
-			foreach (var item in range)
-			{
-				collection.Insert(index, item);
-				++index;
-			}
-		}
-	}
+    public static class CollectionExtensions
+    {
+        public static void InsertRange<T>(this Collection<T> collection, int index, IEnumerable<T> range)
+        {
+            foreach (var item in range)
+            {
+                collection.Insert(index, item);
+                ++index;
+            }
+        }
+    }
 }
